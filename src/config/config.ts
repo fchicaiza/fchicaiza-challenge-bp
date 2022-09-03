b7fcd439acd3b186927a7aab9999ffdfa7fe7b82
fchicaiza <fchicaiza@sofyasystemssa.com>
2022-09-03 11:14:51 -0500

    Initial enviroment configuration, switch between develompent and production enviroments

---
+++ src/config/config.ts
@@ -0,0 +1,28 @@
+import * as dotenv from "dotenv";
+
+export abstract class ConfigServer {
+  constructor() {
+    const nodeNameEnv = this.createPathEnv(this.nodeEnv);
+    dotenv.config({
+      path: nodeNameEnv,
+    });
+  }
+  public getEnviroment(k: string): string | undefined {
+    return process.env[k];
+  }
+  public getNumberEnv(k: string): number {
+    // Read enviroment variable and return number
+    return Number(this.getEnviroment(k));
+  }
+  public get nodeEnv(): string {
+    return this.getEnviroment("NODE_ENV")?.trim() || ""; // Read environment variable and always return a string
+  }
+  public createPathEnv(path: string): string {
+    const arrEnv: Array<string> = ["env"]; // Transform array into string chain with custom format
+    if (path.length > 0) {
+      const strigToArray = path.split(".");
+      arrEnv.unshift(...strigToArray);
+    }
+    return "." + arrEnv.join(".");
+  }
+}
